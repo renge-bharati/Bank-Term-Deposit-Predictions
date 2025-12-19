f1087c494596a8351df18cd0a19896572568b764
renge-bharati <rengebharati@gmail.com>
2025-12-19 14:34:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ import pickle
 from tensorflow.keras.models import load_model
 
 # Load trained model and scaler
-model = load_model("model.h5")
-scaler = pickle.load(open("scaler.pkl", "rb"))
+model = load_model("ann_model.pkl")
+scaler = pickle.load(open("ann_model.pkl", "rb"))
 
 st.set_page_config(page_title="Bank Term Deposit Prediction", layout="centered")
 
